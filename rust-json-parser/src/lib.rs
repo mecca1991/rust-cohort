bcf22b4c04b4ff81ee3d33c6d5a35f669e0b1603
wilfredchetat <yekuwilfred@gmail.com>
2026-09-17 22:32:34 +0400

    implement first test case for tokenize function to handle LeftBrace and RightBrace

--- rust-json-parser/src/lib.rs
+++ rust-json-parser/src/lib.rs
@@ -1,3 +1,5 @@
+pub mod tokenizer;
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
@@ -5,10 +7,21 @@ pub fn add(left: u64, right: u64) -> u64 {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use crate::tokenizer::Token;
+    use crate::tokenizer::tokenize;
 
     #[test]
     fn it_works() {
         let result = add(2, 2);
         assert_eq!(result, 4);
     }
+
+     #[test]
+    fn test_empty_braes() {
+        let tokens = tokenize("{}");
+        assert_eq!(tokens.len(), 2);
+        assert_eq!(tokens[0], Token::LeftBrace);
+        assert_eq!(tokens[1], Token::RightBrace);
+    }
 }
+
---
+++ rust-json-parser/src/tokenizer.rs
@@ -0,0 +1,35 @@
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Token {
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Comma,
+    Colon,
+    String,
+    Number,
+    Boolean
+}
+
+pub fn tokenize(input: &str) -> Vec<Token> {
+    let mut tokens: Vec<Token> = Vec::new();
+    let mut chars = input.chars().peekable();
+    while let Some(&ch) = chars.peek() {
+        match ch {
+            '{' => {
+                tokens.push(Token::LeftBrace);
+                chars.next();
+            },
+            '}' => {
+                tokens.push(Token::RightBrace);
+                chars.next();
+
+            },
+            _ => {
+                println!("Nothing Done");
+            }
+        }
+    }
+    return tokens;
+}
